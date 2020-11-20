102cc484010b09f55647f1c704f87f2925440850
JosedeJesusMI <jchuchinislas74@gmail.com>
2020-11-19 20:07:25 -0600

    pract

---
+++ src/app/components/tarea/tarea.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
+
+@Component({
+  selector: 'app-tarea',
+  templateUrl: './tarea.component.html'
+})
+export class TareaComponent implements OnInit {
+  heroe:any = {};
+  constructor( private activatedRoute: ActivatedRoute,
+    private _heroesService: HeroesService
+    ) { 
+      this.activatedRoute.params.subscribe( params =>{
+        // console.log(params ['id']);
+        this.heroe = this._heroesService.getHeroe(params['id']);
+       })
+
+    }
+
+  ngOnInit(): void {
+  }
+
+}
